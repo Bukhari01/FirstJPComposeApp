dc7b38555b5f6a053f013bc1eb8788f82990142b
nafeyJaved <nafeyjaved10@gmail.com>
2025-07-23 11:45:43 +0500

    Tonal button with onClick()

--- app/src/main/java/com/example/firstjpcomposeapp/3_CoreComponents/D_ButtonFile.kt
+++ app/src/main/java/com/example/firstjpcomposeapp/3_CoreComponents/D_ButtonFile.kt
@@ -3,7 +3,8 @@ package com.example.firstjpcomposeapp.`3_CoreComponents`
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+//import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,33 +14,49 @@ import androidx.compose.ui.tooling.preview.Preview
 
 //Filled Button
 
+//@Composable
+//fun FilledButtonSample() {
+//
+//    Box(
+//        modifier = Modifier.fillMaxSize(),
+//        contentAlignment = Alignment.Center
+//    ) {
+//        val context = LocalContext.current
+//        Button(
+//            onClick = {
+//                Toast.makeText(context,"Button is clicked",Toast.LENGTH_SHORT).show()
+//            }
+//        ) {
+//            Text("Filled Button")
+//        }
+//    }
+//}
 
 
+// Tonal Button
 
 @Composable
-fun FilledButtonSample() {
-
+fun FilledTonalButton(){
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        val context = LocalContext.current
-        Button(
+       val context = LocalContext.current
+
+        FilledTonalButton(
             onClick = {
                 Toast.makeText(context,"Button is clicked",Toast.LENGTH_SHORT).show()
             }
         ) {
-            Text("Filled Button")
+            Text("Tonal Button")
         }
     }
 }
 
 
 
-
-
 @Preview(showSystemUi = true)
 @Composable
 fun PreviewFilled(){
-    FilledButtonSample()
+    FilledTonalButton()
 }

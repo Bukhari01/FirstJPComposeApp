bcfc24e13dff7e17d1862fa417445f86424737c8
nafeyJaved <nafeyjaved10@gmail.com>
2025-07-24 11:38:36 +0500

    Constraint layout

--- app/src/main/java/com/example/firstjpcomposeapp/4_LayoutComposables/Layouts.kt
+++ app/src/main/java/com/example/firstjpcomposeapp/4_LayoutComposables/Layouts.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 //import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
@@ -18,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 
 //Column allows you to place its children in a vertical sequence.
@@ -72,9 +74,51 @@ fun BoxExample() {
     }
 }
 
+//Constraint layout -> powerful, but effects performance, costly, complex
+
+@Composable
+fun Constraint() {
+    Column {
+        ConstraintLayout(
+            modifier = Modifier.fillMaxWidth()
+                .height(100.dp)
+                .background(color = Color.LightGray)
+        ) {
+            val (text1,text2,text3) = createRefs()
+            Text("Bottom Left",
+                modifier = Modifier.constrainAs(text1){
+                    bottom.linkTo( parent.bottom, margin = 8.dp )
+                    start.linkTo(parent.start, margin = 8.dp)
+                }
+            )
+            Text("Center Left",
+                modifier = Modifier.constrainAs(text2){
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+            Text("Top Right",
+                modifier = Modifier.constrainAs(text3){
+                    top.linkTo(parent.top)
+//                    bottom.linkTo(parent.bottom)
+//                    start.linkTo(parent.start)
+                    end.linkTo(parent.end, margin = 8.dp)
+                }
+            )
+        }
+    }
+}
+
 
 @Preview(showSystemUi = true)
 @Composable
 fun Show(){
-    BoxExample()
+    Constraint()
 }
+
+
+//Best Practices
+
+//1. Over Nesting (not more than 5)

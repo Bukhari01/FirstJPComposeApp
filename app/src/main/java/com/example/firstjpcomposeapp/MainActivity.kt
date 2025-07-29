88ac0e3ed7b2229811bb030455f08c5d6633dc57
nafeyJaved <nafeyjaved10@gmail.com>
2025-07-29 12:23:40 +0500

    navigation drawer sheet

--- app/src/main/java/com/example/firstjpcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/firstjpcomposeapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.firstjpcomposeapp.`11_NavigationDrawer`.DetailedDrawerExample
 import com.example.firstjpcomposeapp.`2_FundamentalConcepts`.AccessStringResource
 import com.example.firstjpcomposeapp.`2_FundamentalConcepts`.Greeting
 import com.example.firstjpcomposeapp.`3_CoreComponents`.PartiallySelectable
@@ -24,7 +25,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FirstJPComposeAppTheme {
-                CheckBoxExample()
+                DetailedDrawerExample()
 
             }
         }
